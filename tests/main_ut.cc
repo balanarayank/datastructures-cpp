224913e49465324f5fdbbce253970303fc581eb1
balanarayank <28087097+balanarayank@users.noreply.github.com>
2022-05-17 20:58:15 +0530

    Cleaning up for re-writing more simple code

--- tests/main_ut.cc
+++
@@ -1,7 +0,0 @@
-#include "SinglyLinkedList/SinglyLinkedList_ut.h"
-#include "DoublyLinkedList/DoublyLinkedList_ut.h"
-
-int main(int argc, char* argv[]) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
