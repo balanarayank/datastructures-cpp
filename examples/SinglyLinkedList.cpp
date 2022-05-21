3a73b210848ca6874c2499e17fd7a8c2101735e4
balanarayank <28087097+balanarayank@users.noreply.github.com>
2022-05-21 12:47:34 +0530

    1. Added more test cases 2. Improved implementation

--- examples/SinglyLinkedList.cpp
+++ examples/SinglyLinkedList.cpp
@@ -2,67 +2,71 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    SinglyLinkedList<int> sllIntObj;
-    sllIntObj.InsertAtHead(1);
-    sllIntObj.InsertAtHead(3);
-    sllIntObj.InsertAtTail(2);
-    sllIntObj.InsertAtTail(4);
-    sllIntObj.InsertAtHead(5);
-    std::cout << "First Node Data: " << sllIntObj.GetFirstNode()->data << std::endl;
-    std::cout << "Last Node Data: " << sllIntObj.GetLastNode()->data << std::endl;
-    std::cout << "Linked List: " << sllIntObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllIntObj.GetSize() << std::endl;
+    LinkedList<int> *sllIntObj = new SinglyLinkedList<int>;
+    sllIntObj->InsertAtHead(1);
+    sllIntObj->InsertAtHead(3);
+    sllIntObj->InsertAtTail(2);
+    sllIntObj->InsertAtTail(4);
+    sllIntObj->InsertAtHead(5);
+    std::cout << "First Node Data: " << sllIntObj->GetFirstNode()->data << std::endl;
+    std::cout << "Last Node Data: " << sllIntObj->GetLastNode()->data << std::endl;
+    std::cout << "Linked List: " << sllIntObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllIntObj->GetSize() << std::endl;
     
-    if(sllIntObj.DeleteData(5)) {
+    if(sllIntObj->DeleteData(5)) {
         std::cout << "Deleted 5\n";
     }
-    std::cout << "First Node Data: " << sllIntObj.GetFirstNode()->data << std::endl;
-    std::cout << "Last Node Data: " << sllIntObj.GetLastNode()->data << std::endl;
-    std::cout << "Linked List: " << sllIntObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllIntObj.GetSize() << std::endl;
+    std::cout << "First Node Data: " << sllIntObj->GetFirstNode()->data << std::endl;
+    std::cout << "Last Node Data: " << sllIntObj->GetLastNode()->data << std::endl;
+    std::cout << "Linked List: " << sllIntObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllIntObj->GetSize() << std::endl;
     
-    if(sllIntObj.DeleteData(1)) {
+    if(sllIntObj->DeleteData(1)) {
         std::cout << "Deleted 1\n";
     }
-    if(sllIntObj.DeleteData(100)) {
+    if(sllIntObj->DeleteData(100)) {
         std::cout << "Deleted 100\n";
     }
-    std::cout << "First Node Data: " << sllIntObj.GetFirstNode()->data << std::endl;
-    std::cout << "Last Node Data: " << sllIntObj.GetLastNode()->data << std::endl;
-    std::cout << "Linked List: " << sllIntObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllIntObj.GetSize() << std::endl;
+    std::cout << "First Node Data: " << sllIntObj->GetFirstNode()->data << std::endl;
+    std::cout << "Last Node Data: " << sllIntObj->GetLastNode()->data << std::endl;
+    std::cout << "Linked List: " << sllIntObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllIntObj->GetSize() << std::endl;
 
-    sllIntObj.InsertAtTail(6);
-    sllIntObj.InsertAtHead(7);
-    std::cout << "First Node Data: " << sllIntObj.GetFirstNode()->data << std::endl;
-    std::cout << "Last Node Data: " << sllIntObj.GetLastNode()->data << std::endl;
-    std::cout << "Linked List: " << sllIntObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllIntObj.GetSize() << std::endl;
+    sllIntObj->InsertAtTail(6);
+    sllIntObj->InsertAtHead(7);
+    std::cout << "First Node Data: " << sllIntObj->GetFirstNode()->data << std::endl;
+    std::cout << "Last Node Data: " << sllIntObj->GetLastNode()->data << std::endl;
+    std::cout << "Linked List: " << sllIntObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllIntObj->GetSize() << std::endl;
 
-    if(sllIntObj.DeleteData(6)) {
+    if(sllIntObj->DeleteData(6)) {
         std::cout << "Deleted 6\n";
     }
-    std::cout << "First Node Data: " << sllIntObj.GetFirstNode()->data << std::endl;
-    std::cout << "Last Node Data: " << sllIntObj.GetLastNode()->data << std::endl;
-    std::cout << "Linked List: " << sllIntObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllIntObj.GetSize() << std::endl;
+    std::cout << "First Node Data: " << sllIntObj->GetFirstNode()->data << std::endl;
+    std::cout << "Last Node Data: " << sllIntObj->GetLastNode()->data << std::endl;
+    std::cout << "Linked List: " << sllIntObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllIntObj->GetSize() << std::endl;
 
-    if(sllIntObj.DeleteNode(sllIntObj.GetFirstNode()->next->next)) {
+    if(sllIntObj->DeleteNode(sllIntObj->GetFirstNode()->next->next)) {
         std::cout << "Deleted node\n";
     }
-    std::cout << "First Node Data: " << sllIntObj.GetFirstNode()->data << std::endl;
-    std::cout << "Last Node Data: " << sllIntObj.GetLastNode()->data << std::endl;
-    std::cout << "Linked List: " << sllIntObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllIntObj.GetSize() << std::endl;
-    std::cout << "Linked List in reverse: " << sllIntObj.DisplayReverse() << std::endl;
+    std::cout << "First Node Data: " << sllIntObj->GetFirstNode()->data << std::endl;
+    std::cout << "Last Node Data: " << sllIntObj->GetLastNode()->data << std::endl;
+    std::cout << "Linked List: " << sllIntObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllIntObj->GetSize() << std::endl;
+    std::cout << "Linked List in reverse: " << sllIntObj->DisplayReverse() << std::endl;
 
-    SinglyLinkedList<std::string> sllStrObj;
-    sllStrObj.InsertAtHead("Hello");
-    std::cout << "Linked List: " << sllStrObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllStrObj.GetSize() << std::endl;
-    sllStrObj.DeleteData("Hello");
-    std::cout << "Linked List: " << sllStrObj.Display() << std::endl;
-    std::cout << "Linked List Size: " << sllStrObj.GetSize() << std::endl;
+    delete sllIntObj;
+
+    LinkedList<std::string> *sllStrObj = new SinglyLinkedList<std::string>;
+    sllStrObj->InsertAtHead("Hello");
+    std::cout << "Linked List: " << sllStrObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllStrObj->GetSize() << std::endl;
+    sllStrObj->DeleteData("Hello");
+    std::cout << "Linked List: " << sllStrObj->Display() << std::endl;
+    std::cout << "Linked List Size: " << sllStrObj->GetSize() << std::endl;
+
+    delete sllStrObj;
     
     return EXIT_SUCCESS;
 }
--- tests/SinglyLinkedList_ut.h
+++ tests/SinglyLinkedList_ut.h
@@ -5,216 +5,261 @@
 #include "SinglyLinkedList.h"
 
 TEST(SinglyLinkedListIntTest, EmptyLinkedList) {
-    SinglyLinkedList<int> sllIntObj;
-    ASSERT_EQ(sllIntObj.GetFirstNode(), nullptr);
-    ASSERT_EQ(sllIntObj.GetLastNode(), nullptr);
-    ASSERT_EQ(sllIntObj.GetSize(), 0);
-    ASSERT_EQ(sllIntObj.DeleteData(2), false);
-    ASSERT_EQ(sllIntObj.Display(), "SinglyLinkedList is empty");
-    ASSERT_EQ(sllIntObj.DisplayReverse(), "Not supported for SinglyLinkedList");
+    LinkedList<int> *sllIntObj = new SinglyLinkedList<int>;
+    ASSERT_EQ(sllIntObj->GetFirstNode(), nullptr);
+    ASSERT_EQ(sllIntObj->GetLastNode(), nullptr);
+    ASSERT_EQ(sllIntObj->GetSize(), 0);
+    ASSERT_EQ(sllIntObj->DeleteData(2), false);
+    ASSERT_EQ(sllIntObj->Display(), "SinglyLinkedList is empty");
+    ASSERT_EQ(sllIntObj->DisplayReverse(), "Not supported for SinglyLinkedList");
+    delete sllIntObj;
 }
 
 TEST(SinglyLinkedListIntTest, InsertAtHeadAndDeleteData) {
-    SinglyLinkedList<int> sllIntObj;
-    sllIntObj.InsertAtHead(1);
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 1);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 1);
-    ASSERT_EQ(sllIntObj.GetSize(), 1);
-    ASSERT_EQ(sllIntObj.Display(), "1 -> ");
+    LinkedList<int> *sllIntObj = new SinglyLinkedList<int>;
+    sllIntObj->InsertAtHead(1);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 1);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 1);
+    ASSERT_EQ(sllIntObj->GetSize(), 1);
+    ASSERT_EQ(sllIntObj->Display(), "1 -> ");
     
-    ASSERT_EQ(sllIntObj.DeleteData(2), false);
-    ASSERT_EQ(sllIntObj.DeleteData(1), true);
-    ASSERT_EQ(sllIntObj.GetFirstNode(), nullptr);
-    ASSERT_EQ(sllIntObj.GetLastNode(), nullptr);
-    ASSERT_EQ(sllIntObj.GetSize(), 0);
-    ASSERT_EQ(sllIntObj.Display(), "SinglyLinkedList is empty");
+    ASSERT_EQ(sllIntObj->DeleteData(2), false);
+    ASSERT_EQ(sllIntObj->DeleteData(1), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode(), nullptr);
+    ASSERT_EQ(sllIntObj->GetLastNode(), nullptr);
+    ASSERT_EQ(sllIntObj->GetSize(), 0);
+    ASSERT_EQ(sllIntObj->Display(), "SinglyLinkedList is empty");
+    delete sllIntObj;
 }
 
 TEST(SinglyLinkedListIntTest, InsertAtTailAndDeleteData) {
-    SinglyLinkedList<int> sllIntObj;
-    sllIntObj.InsertAtTail(10);
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 10);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 10);
-    ASSERT_EQ(sllIntObj.GetSize(), 1);
-    ASSERT_EQ(sllIntObj.Display(), "10 -> ");
+    LinkedList<int> *sllIntObj = new SinglyLinkedList<int>;
+    sllIntObj->InsertAtTail(10);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 10);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 10);
+    ASSERT_EQ(sllIntObj->GetSize(), 1);
+    ASSERT_EQ(sllIntObj->Display(), "10 -> ");
     
-    ASSERT_EQ(sllIntObj.DeleteData(20), false);
-    ASSERT_EQ(sllIntObj.DeleteData(10), true);
-    ASSERT_EQ(sllIntObj.GetFirstNode(), nullptr);
-    ASSERT_EQ(sllIntObj.GetLastNode(), nullptr);
-    ASSERT_EQ(sllIntObj.GetSize(), 0);
-    ASSERT_EQ(sllIntObj.Display(), "SinglyLinkedList is empty");
+    ASSERT_EQ(sllIntObj->DeleteData(20), false);
+    ASSERT_EQ(sllIntObj->DeleteData(10), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode(), nullptr);
+    ASSERT_EQ(sllIntObj->GetLastNode(), nullptr);
+    ASSERT_EQ(sllIntObj->GetSize(), 0);
+    ASSERT_EQ(sllIntObj->Display(), "SinglyLinkedList is empty");
+    delete sllIntObj;
 }
 
 class SinglyLinkedListIntTestF : public ::testing::Test
 {
   protected:
-    SinglyLinkedList<int> sllIntObj;
+    LinkedList<int> *sllIntObj;
 
     virtual void SetUp() {
-        sllIntObj.InsertAtHead(1);
-        sllIntObj.InsertAtHead(2);
-        sllIntObj.InsertAtHead(3);
-        sllIntObj.InsertAtTail(4);
-        sllIntObj.InsertAtTail(5);
-        sllIntObj.InsertAtHead(6);
-        sllIntObj.InsertAtTail(7);
-        sllIntObj.InsertAtHead(8);
+        sllIntObj = new SinglyLinkedList<int>;
+        sllIntObj->InsertAtHead(1);
+        sllIntObj->InsertAtHead(2);
+        sllIntObj->InsertAtHead(3);
+        sllIntObj->InsertAtTail(4);
+        sllIntObj->InsertAtTail(5);
+        sllIntObj->InsertAtHead(6);
+        sllIntObj->InsertAtTail(7);
+        sllIntObj->InsertAtHead(8);
     }
 
     virtual void TearDown() {
-
+        delete sllIntObj;
     }
 };
 
 TEST_F(SinglyLinkedListIntTestF, InsertDataAndPrint) {
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 8);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 7);
-    ASSERT_EQ(sllIntObj.GetSize(), 8);
-    ASSERT_EQ(sllIntObj.Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 4 -> 5 -> 7 -> ");
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 8);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 7);
+    ASSERT_EQ(sllIntObj->GetSize(), 8);
+    ASSERT_EQ(sllIntObj->Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 4 -> 5 -> 7 -> ");
 }
 
 TEST_F(SinglyLinkedListIntTestF, DeleteDataAtHead) {
-    ASSERT_EQ(sllIntObj.DeleteData(8), true);
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 6);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 7);
-    ASSERT_EQ(sllIntObj.GetSize(), 7);
-    ASSERT_EQ(sllIntObj.Display(), "6 -> 3 -> 2 -> 1 -> 4 -> 5 -> 7 -> ");
+    ASSERT_EQ(sllIntObj->DeleteData(8), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 6);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 7);
+    ASSERT_EQ(sllIntObj->GetSize(), 7);
+    ASSERT_EQ(sllIntObj->Display(), "6 -> 3 -> 2 -> 1 -> 4 -> 5 -> 7 -> ");
+}
+
+TEST_F(SinglyLinkedListIntTestF, DeleteNodeAtHead) {
+    ASSERT_EQ(sllIntObj->DeleteNode(sllIntObj->GetFirstNode()), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 6);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 7);
+    ASSERT_EQ(sllIntObj->GetSize(), 7);
+    ASSERT_EQ(sllIntObj->Display(), "6 -> 3 -> 2 -> 1 -> 4 -> 5 -> 7 -> ");
 }
 
 TEST_F(SinglyLinkedListIntTestF, DeleteDataAtTail) {
-    ASSERT_EQ(sllIntObj.DeleteData(7), true);
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 8);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 5);
-    ASSERT_EQ(sllIntObj.GetSize(), 7);
-    ASSERT_EQ(sllIntObj.Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 4 -> 5 -> ");
+    ASSERT_EQ(sllIntObj->DeleteData(7), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 8);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 5);
+    ASSERT_EQ(sllIntObj->GetSize(), 7);
+    ASSERT_EQ(sllIntObj->Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 4 -> 5 -> ");
+}
+
+TEST_F(SinglyLinkedListIntTestF, DeleteNodeAtTail) {
+    ASSERT_EQ(sllIntObj->DeleteNode(sllIntObj->GetLastNode()), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 8);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 5);
+    ASSERT_EQ(sllIntObj->GetSize(), 7);
+    ASSERT_EQ(sllIntObj->Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 4 -> 5 -> ");
 }
 
 TEST_F(SinglyLinkedListIntTestF, DeleteDataInside) {
-    ASSERT_EQ(sllIntObj.DeleteData(4), true);
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 8);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 7);
-    ASSERT_EQ(sllIntObj.GetSize(), 7);
-    ASSERT_EQ(sllIntObj.Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 5 -> 7 -> ");
+    ASSERT_EQ(sllIntObj->DeleteData(4), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 8);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 7);
+    ASSERT_EQ(sllIntObj->GetSize(), 7);
+    ASSERT_EQ(sllIntObj->Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 5 -> 7 -> ");
+}
+
+TEST_F(SinglyLinkedListIntTestF, DeleteNodeInside) {
+    Node<int> *llNode = sllIntObj->GetFirstNode();
+    ASSERT_EQ(sllIntObj->DeleteNode(llNode->next->next->next->next->next), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 8);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 7);
+    ASSERT_EQ(sllIntObj->GetSize(), 7);
+    ASSERT_EQ(sllIntObj->Display(), "8 -> 6 -> 3 -> 2 -> 1 -> 5 -> 7 -> ");
 }
 
 TEST_F(SinglyLinkedListIntTestF, DeleteMultipleData) {
-    ASSERT_EQ(sllIntObj.DeleteData(4), true);
-    ASSERT_EQ(sllIntObj.DeleteData(8), true);
-    ASSERT_EQ(sllIntObj.DeleteData(6), true);
-    ASSERT_EQ(sllIntObj.DeleteData(7), true);
-    ASSERT_EQ(sllIntObj.GetFirstNode()->data, 3);
-    ASSERT_EQ(sllIntObj.GetLastNode()->data, 5);
-    ASSERT_EQ(sllIntObj.GetSize(), 4);
-    ASSERT_EQ(sllIntObj.Display(), "3 -> 2 -> 1 -> 5 -> ");
+    ASSERT_EQ(sllIntObj->DeleteData(4), true);
+    ASSERT_EQ(sllIntObj->DeleteData(8), true);
+    ASSERT_EQ(sllIntObj->DeleteData(6), true);
+    ASSERT_EQ(sllIntObj->DeleteData(7), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 3);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 5);
+    ASSERT_EQ(sllIntObj->GetSize(), 4);
+    ASSERT_EQ(sllIntObj->Display(), "3 -> 2 -> 1 -> 5 -> ");
 }
 
-//==========================================
+TEST_F(SinglyLinkedListIntTestF, DeleteMultipleNodes) {
+    Node<int> *llNode = sllIntObj->GetFirstNode();
+    ASSERT_EQ(sllIntObj->DeleteNode(llNode->next->next->next->next->next), true);
+    ASSERT_EQ(sllIntObj->DeleteNode(llNode), true);
+    llNode = sllIntObj->GetFirstNode();
+    ASSERT_EQ(sllIntObj->DeleteNode(llNode), true);
+    llNode = sllIntObj->GetLastNode();
+    ASSERT_EQ(sllIntObj->DeleteNode(llNode), true);
+    ASSERT_EQ(sllIntObj->GetFirstNode()->data, 3);
+    ASSERT_EQ(sllIntObj->GetLastNode()->data, 5);
+    ASSERT_EQ(sllIntObj->GetSize(), 4);
+    ASSERT_EQ(sllIntObj->Display(), "3 -> 2 -> 1 -> 5 -> ");
+}
 
 TEST(SinglyLinkedListStringTest, EmptyLinkedList) {
-    SinglyLinkedList<std::string> sllStrObj;
-    ASSERT_EQ(sllStrObj.GetFirstNode(), nullptr);
-    ASSERT_EQ(sllStrObj.GetLastNode(), nullptr);
-    ASSERT_EQ(sllStrObj.GetSize(), 0);
-    ASSERT_EQ(sllStrObj.DeleteData("Hello"), false);
-    ASSERT_EQ(sllStrObj.Display(), "SinglyLinkedList is empty");
-    ASSERT_EQ(sllStrObj.DisplayReverse(), "Not supported for SinglyLinkedList");
+    LinkedList<std::string> *sllStrObj = new SinglyLinkedList<std::string>;
+    ASSERT_EQ(sllStrObj->GetFirstNode(), nullptr);
+    ASSERT_EQ(sllStrObj->GetLastNode(), nullptr);
+    ASSERT_EQ(sllStrObj->GetSize(), 0);
+    ASSERT_EQ(sllStrObj->DeleteData("Hello"), false);
+    ASSERT_EQ(sllStrObj->Display(), "SinglyLinkedList is empty");
+    ASSERT_EQ(sllStrObj->DisplayReverse(), "Not supported for SinglyLinkedList");
+    delete sllStrObj;
 }
 
 TEST(SinglyLinkedListStringTest, InsertAtHeadAndDeleteData) {
-    SinglyLinkedList<std::string> sllStrObj;
-    sllStrObj.InsertAtHead("Hello");
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "Hello");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "Hello");
-    ASSERT_EQ(sllStrObj.GetSize(), 1);
-    ASSERT_EQ(sllStrObj.Display(), "Hello -> ");
+    LinkedList<std::string> *sllStrObj = new SinglyLinkedList<std::string>;
+    sllStrObj->InsertAtHead("Hello");
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "Hello");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "Hello");
+    ASSERT_EQ(sllStrObj->GetSize(), 1);
+    ASSERT_EQ(sllStrObj->Display(), "Hello -> ");
     
-    ASSERT_EQ(sllStrObj.DeleteData("World"), false);
-    ASSERT_EQ(sllStrObj.DeleteData("Hello"), true);
-    ASSERT_EQ(sllStrObj.GetFirstNode(), nullptr);
-    ASSERT_EQ(sllStrObj.GetLastNode(), nullptr);
-    ASSERT_EQ(sllStrObj.GetSize(), 0);
-    ASSERT_EQ(sllStrObj.Display(), "SinglyLinkedList is empty");
+    ASSERT_EQ(sllStrObj->DeleteData("World"), false);
+    ASSERT_EQ(sllStrObj->DeleteData("Hello"), true);
+    ASSERT_EQ(sllStrObj->GetFirstNode(), nullptr);
+    ASSERT_EQ(sllStrObj->GetLastNode(), nullptr);
+    ASSERT_EQ(sllStrObj->GetSize(), 0);
+    ASSERT_EQ(sllStrObj->Display(), "SinglyLinkedList is empty");
+    delete sllStrObj;
 }
 
 TEST(SinglyLinkedListStringTest, InsertAtTailAndDeleteData) {
-    SinglyLinkedList<std::string> sllStrObj;
-    sllStrObj.InsertAtTail("World");
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "World");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "World");
-    ASSERT_EQ(sllStrObj.GetSize(), 1);
-    ASSERT_EQ(sllStrObj.Display(), "World -> ");
+    LinkedList<std::string> *sllStrObj = new SinglyLinkedList<std::string>;
+    sllStrObj->InsertAtTail("World");
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "World");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "World");
+    ASSERT_EQ(sllStrObj->GetSize(), 1);
+    ASSERT_EQ(sllStrObj->Display(), "World -> ");
     
-    ASSERT_EQ(sllStrObj.DeleteData("Hello"), false);
-    ASSERT_EQ(sllStrObj.DeleteData("World"), true);
-    ASSERT_EQ(sllStrObj.GetFirstNode(), nullptr);
-    ASSERT_EQ(sllStrObj.GetLastNode(), nullptr);
-    ASSERT_EQ(sllStrObj.GetSize(), 0);
-    ASSERT_EQ(sllStrObj.Display(), "SinglyLinkedList is empty");
+    ASSERT_EQ(sllStrObj->DeleteData("Hello"), false);
+    ASSERT_EQ(sllStrObj->DeleteData("World"), true);
+    ASSERT_EQ(sllStrObj->GetFirstNode(), nullptr);
+    ASSERT_EQ(sllStrObj->GetLastNode(), nullptr);
+    ASSERT_EQ(sllStrObj->GetSize(), 0);
+    ASSERT_EQ(sllStrObj->Display(), "SinglyLinkedList is empty");
+    delete sllStrObj;
 }
 
 class SinglyLinkedListStringTestF : public ::testing::Test
 {
   protected:
-    SinglyLinkedList<std::string> sllStrObj;
+    LinkedList<std::string> *sllStrObj;
 
     virtual void SetUp() {
-        sllStrObj.InsertAtHead("Hello");
-        sllStrObj.InsertAtHead("World");
-        sllStrObj.InsertAtHead("Good");
-        sllStrObj.InsertAtTail("Morning");
-        sllStrObj.InsertAtTail("Test");
-        sllStrObj.InsertAtHead("This");
-        sllStrObj.InsertAtTail("Great");
-        sllStrObj.InsertAtHead("Day");
+        sllStrObj = new SinglyLinkedList<std::string>;
+        sllStrObj->InsertAtHead("Hello");
+        sllStrObj->InsertAtHead("World");
+        sllStrObj->InsertAtHead("Good");
+        sllStrObj->InsertAtTail("Morning");
+        sllStrObj->InsertAtTail("Test");
+        sllStrObj->InsertAtHead("This");
+        sllStrObj->InsertAtTail("Great");
+        sllStrObj->InsertAtHead("Day");
     }
 
     virtual void TearDown() {
-
+        delete sllStrObj;
     }
 };
 
 TEST_F(SinglyLinkedListStringTestF, InsertDataAndPrint) {
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "Day");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "Great");
-    ASSERT_EQ(sllStrObj.GetSize(), 8);
-    ASSERT_EQ(sllStrObj.Display(), "Day -> This -> Good -> World -> Hello -> Morning -> Test -> Great -> ");
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "Day");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "Great");
+    ASSERT_EQ(sllStrObj->GetSize(), 8);
+    ASSERT_EQ(sllStrObj->Display(), "Day -> This -> Good -> World -> Hello -> Morning -> Test -> Great -> ");
 }
 
 TEST_F(SinglyLinkedListStringTestF, DeleteDataAtHead) {
-    ASSERT_EQ(sllStrObj.DeleteData("Day"), true);
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "This");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "Great");
-    ASSERT_EQ(sllStrObj.GetSize(), 7);
-    ASSERT_EQ(sllStrObj.Display(), "This -> Good -> World -> Hello -> Morning -> Test -> Great -> ");
+    ASSERT_EQ(sllStrObj->DeleteData("Day"), true);
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "This");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "Great");
+    ASSERT_EQ(sllStrObj->GetSize(), 7);
+    ASSERT_EQ(sllStrObj->Display(), "This -> Good -> World -> Hello -> Morning -> Test -> Great -> ");
 }
 
 TEST_F(SinglyLinkedListStringTestF, DeleteDataAtTail) {
-    ASSERT_EQ(sllStrObj.DeleteData("Great"), true);
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "Day");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "Test");
-    ASSERT_EQ(sllStrObj.GetSize(), 7);
-    ASSERT_EQ(sllStrObj.Display(), "Day -> This -> Good -> World -> Hello -> Morning -> Test -> ");
+    ASSERT_EQ(sllStrObj->DeleteData("Great"), true);
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "Day");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "Test");
+    ASSERT_EQ(sllStrObj->GetSize(), 7);
+    ASSERT_EQ(sllStrObj->Display(), "Day -> This -> Good -> World -> Hello -> Morning -> Test -> ");
 }
 
 TEST_F(SinglyLinkedListStringTestF, DeleteDataInside) {
-    ASSERT_EQ(sllStrObj.DeleteData("Morning"), true);
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "Day");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "Great");
-    ASSERT_EQ(sllStrObj.GetSize(), 7);
-    ASSERT_EQ(sllStrObj.Display(), "Day -> This -> Good -> World -> Hello -> Test -> Great -> ");
+    ASSERT_EQ(sllStrObj->DeleteData("Morning"), true);
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "Day");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "Great");
+    ASSERT_EQ(sllStrObj->GetSize(), 7);
+    ASSERT_EQ(sllStrObj->Display(), "Day -> This -> Good -> World -> Hello -> Test -> Great -> ");
 }
 
 TEST_F(SinglyLinkedListStringTestF, DeleteMultipleData) {
-    ASSERT_EQ(sllStrObj.DeleteData("Good"), true);
-    ASSERT_EQ(sllStrObj.DeleteData("Hello"), true);
-    ASSERT_EQ(sllStrObj.DeleteData("Day"), true);
-    ASSERT_EQ(sllStrObj.DeleteData("This"), true);
-    ASSERT_EQ(sllStrObj.DeleteData("Great"), true);
-    ASSERT_EQ(sllStrObj.GetFirstNode()->data, "World");
-    ASSERT_EQ(sllStrObj.GetLastNode()->data, "Test");
-    ASSERT_EQ(sllStrObj.GetSize(), 3);
-    ASSERT_EQ(sllStrObj.Display(), "World -> Morning -> Test -> ");
+    ASSERT_EQ(sllStrObj->DeleteData("Good"), true);
+    ASSERT_EQ(sllStrObj->DeleteData("Hello"), true);
+    ASSERT_EQ(sllStrObj->DeleteData("Day"), true);
+    ASSERT_EQ(sllStrObj->DeleteData("This"), true);
+    ASSERT_EQ(sllStrObj->DeleteData("Great"), true);
+    ASSERT_EQ(sllStrObj->GetFirstNode()->data, "World");
+    ASSERT_EQ(sllStrObj->GetLastNode()->data, "Test");
+    ASSERT_EQ(sllStrObj->GetSize(), 3);
+    ASSERT_EQ(sllStrObj->Display(), "World -> Morning -> Test -> ");
 }
 
 #endif
